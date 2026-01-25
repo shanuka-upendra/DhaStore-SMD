c2cb31e9efdca440c8a3390511b4250e0e938658
ShanukaUpendraOfficial <shanukaupendra@gmail.com>
2026-01-25 12:46:40 +0530

    initial commit

---
+++ src/main/java/edu/icet/model/Product.java
@@ -0,0 +1,18 @@
+package edu.icet.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Product {
+    private String name;
+    private double price;
+    private String description;
+    private String image;
+
+}
