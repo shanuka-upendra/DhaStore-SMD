a52a70c3a7835097fc3dcc15dea814f8a8597eb6
ShanukaUpendraOfficial <shanukaupendra@gmail.com>
2026-02-05 10:41:55 +0530

    feat: add Flyway dependency for database migrations

--- src/main/java/edu/icet/controller/ProductController.java
+++ src/main/java/edu/icet/controller/ProductController.java
@@ -1,4 +1,10 @@
 package edu.icet.controller;
 
+import edu.icet.model.Product;
+
 public class ProductController {
+
+    public Product getProduct(){
+        return new Product("Phone",5000.0,"IPhone 14 Pro","-");
+    }
 }
